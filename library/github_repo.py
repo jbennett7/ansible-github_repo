22a26c836c29df4e88fbdf571a2eb62d50742baa
Joseph Bennett <jbennett@epix.com>
2017-07-23 21:21:38 -0400

    first working version for using just requests

--- library/github_repo.py
+++ library/github_repo.py
@@ -7,30 +7,45 @@ import json
 import requests
 import re
 
+CONFIGURABLE_KEY_LIST = [ 
+    'name',
+    'description',
+    'private',
+    'has_issues',
+    'has_projects',
+    'has_wiki',
+    'homepage',
+    'allow_squash_merge',
+    'allow_merge_commit',
+    'allow_rebase_merge']
+
 class AuthenticationError(Exception):
     pass
 
 class ImmutableLabels(Exception):
     pass
 
+class HTMLError(Exception):
+    pass
+
 class GithubRepository(object):
     def __init__(self, token, org, repo):
-        self._connection = GithubConnection(token, org, repo).get_connection()
         self._header = { 'Authorization': 'token {}'.format(token) }
         self._url = "https://api.github.com/repos/{}/{}".format(org,repo)
-        self._create_repo_url = "https://api.github.com/orgs/{}/repos".format(org)
+        self._org_create_repo_url = "https://api.github.com/orgs/{}/repos".format(org)
+        self._user_create_repo_url = "https://api.github.com/user/repos"
         self._repo = repo
         self._org = org
-        self.create_repository()
 
     def create_repository(self):
         r = requests.get(self._url,headers=self._header)
         if r.status_code != 200:
-            r = requests.post(self._create_repo_url,json.dumps(dict(name=self._repo)))
-            self.exists = True
-            self.changed = True
-        self._config = r.json()
-        return r.status_code
+            r = requests.post(self._org_create_repo_url,data=json.dumps(dict(name=self._repo)),headers=self._header)
+            if r.status_code != 201:
+                r = requests.post(self._user_create_repo_url,data=json.dumps(dict(name=self._repo)),headers=self._header)
+        self._config = dict((k, r.json()[k]) for k in CONFIGURABLE_KEY_LIST)
+        self._labels_url = re.sub('{/name}','',r.json()['labels_url'])
+        return r.status_code, r.text
 
     def get_config(self):
         return self._config
@@ -47,62 +62,48 @@ class GithubRepository(object):
 
     def sync_config(self):
         # Update does not work with allow_squash_merge, allow_rebase_merge, or allow_merge_commits.
-        url = "https://api.github.com/repos/{}/{}".format(self._org,self._repo)
-        r = requests.post(url,json.dumps(self.get_config()),headers=self._header)
-        return "{}:  {}".format(r.text,url)
+        r = requests.post(self._url,json.dumps(self.get_config()),headers=self._header)
+        return r, self._url, self.get_config()
   
     def get_labels(self):
-        label_url = re.sub('{/name}','',self._config['labels_url'])
-        label_list = requests.get(label_url,headers=self._header).json()
+        label_list = requests.get(self._labels_url,headers=self._header).json()
+        return label_list
 
     def labels_equal(self, label_list):
-        cur_label_list = self.get_labels()
-        if len(cur_label_list) != len(label_list):
-            return False
-  
+        current_label_list = [{'name': d['name'],'color': d['color']} for d in self.get_labels()]
+        return current_label_list == label_list
+
     def set_labels(self, label_list):
-        assert isinstance(label_list, list), "label_list must be a list"
-        for item in label_list:
-            assert isinstance(item, dict), "label_list must be a list of dictionaries."
-            assert sorted(item.keys()) == sorted([ 'color', 'name' ]), "invalid keys present."
-            pattern = re.compile("[a-f0-9]{6}")
-            assert pattern.match(item['color']), "<{}> color schema is wrong.".format(item['name'])
-        self._labels = label_list
-  
-    def sync_labels(self):
         deleted_labels=[]
         added_labels=[]
         changed_labels=[]
-        for label in self._connection.issues.labels.list().all():
-            if not any(d["name"] == label.name for d in self._labels):
+        current_label_list = self.get_labels()
+        for label in current_label_list:
+            if not any(d["name"] == label['name'] for d in label_list):
                 # Some repositories have it so that their labels cannot be changed.
-                try:
-                    self._connection.issues.labels.delete(label.name)
-                    deleted_labels.append(label.name)
-                except NotFound:
+                r = requests.delete(label['url'],headers=self._header)
+                if r.status_code != 204:
                     raise ImmutableLabels, "This repository has immutable labels"
-        for label in self._labels:
-            if not any(o.name == label["name"] for o in self._connection.issues.labels.list().all()):
-                self._connection.issues.labels.create(label)
+                deleted_labels.append(label['name'])
+        for label in label_list:
+            if not any(d['name'] == label['name'] for d in current_label_list):
+                r = requests.post(self._labels_url,headers=self._header,data=json.dumps(label))
                 added_labels.append(label['name'])
-        for new_label in self._labels:
-            for old_label in self._connection.issues.labels.list().all():
-                if old_label.name != new_label["name"]: continue
-                if old_label.color != new_label["color"]:
-                    self._connection.issues.labels.update(new_label["name"], new_label)
-                    changed_labels.append(new_label["name"])
+        for new_label in label_list:
+            for old_label in current_label_list:
+                if old_label['name'] == new_label['name']:
+                    if old_label['color'] != new_label['color']:
+                        changed_labels.append(new_label['name'])
+                        r = requests.post(old_label['url'],headers=self._header,data=json.dumps(new_label))
         return deleted_labels, added_labels, changed_labels
   
-    def delete_repo(self):
-        try:
-            self._connection.repos.delete()
-            return True
-        except NotFound:
-            return False
+    def delete_repository(self):
+            return requests.delete(self._url,headers=self._header)
 
 def github_repository_present(params):
     has_changed = False
     is_error = False
+    message = { 'msg': {} }
     token = params['token']
     orgname = params['orgname']
     label_list = params['label_list']
@@ -111,45 +112,38 @@ def github_repository_present(params):
     del params['orgname']
     del params['label_list']
     config_changes = []
-    label_changes = []
     gh = GithubRepository(token, orgname, params['name'])
+    status, results = gh.create_repository()
+    if status == 201:
+        has_changed = True
+        message['msg']['create_repo'] = True
+    elif status == 200:
+        message['msg']['create_repo'] = False
+    else:
+        is_error = True
+        message['msg']['create_repo_error'] = { 'status': status, 'message': results }
     cur_config = gh.get_config()
-    for key in params:
-        if params[key] != cur_config[key]:
-            config_changes.append("{}: {} => {}".format(key, cur_config[key], params[key]))
-            has_changed = True
-    if has_changed == True:
-        gh.set_config(GithubRepositoryConfiguration(
-            name = params['name'],
-            description = params['description'],
-            has_issues = params['has_issues'],
-            has_projects = params['has_projects'],
-            has_wiki = params['has_wiki'],
-            homepage = params['homepage'],
-            allow_squash_merge = params['allow_squash_merge'],
-            allow_merge_commit = params['allow_merge_commit'],
-            allow_rebase_merge = params['allow_rebase_merge'],
-            private = params['private']
-        ))
-        if gh.exists == False:
-            gh.create_repository()
-            has_changed = True
-        result = gh.sync_config()
-    old_labels = gh.get_labels() 
-    if not sorted(old_labels) == sorted(label_list):
-        gh.set_labels(label_list)
-        try:
-            deleted_labels, added_labels, changed_labels = gh.sync_labels()
-            has_changed = True
-            label_changes = { "deleted_labels": deleted_labels, "added_labels": added_labels, "changed_labels": changed_labels }
-        except ImmutableLabels as e:
-            label_changes = { "state": str(e) }
-            has_changed = False
-    try: 
-        result
-    except:
-        result = { "config_changes": config_changes, "label_changes": label_changes }
-    return is_error, has_changed, result
+    if cur_config != params:
+        for key in params.keys():
+            if params[key] != cur_config[key]:
+                config_changes.append("{}: {} => {}".format(key, cur_config[key], params[key]))
+                has_changed = True
+                gh.set_config_var(key, params[key])
+        results, url, config = gh.sync_config()
+        message['msg']['repo_config_changes'] = config_changes
+        message['msg']['repo_config_changes_status'] = results.status_code
+        message['msg']['repo_config_changes_url'] = url
+        message['msg']['repo_config_changes_config'] = config
+    else:
+        message['msg']['repo_config_changes'] = False
+    current_label_list = list(dict((k,label[k]) for k in ['name','color']) for label in gh.get_labels())
+    if label_list != current_label_list:
+        deleted_labels, added_labels, changed_labels = gh.set_labels(label_list)
+        has_changed = True
+        message['msg']['labels_changes'] = { 'deleted_labels': deleted_labels, 'added_labels': added_labels, 'changed_labels': changed_labels }
+    else:
+        message['msg']['labels_changes'] = False
+    return is_error, has_changed, message
 
 def github_repository_absent(params):
     has_changed = False
@@ -162,9 +156,7 @@ def github_repository_absent(params):
     del params['orgname']
     del params['label_list']
     gh = GithubRepository(token, orgname, params['name'])
-    if gh.exists:
-        has_changed = True
-        gh.delete_repo()
+    r = gh.delete_repository()
     return is_error, has_changed, { "msg": "{} deleted.".format(params['name']) }
 
 from ansible.module_utils.basic import *
